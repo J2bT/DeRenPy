69c80a6a8010c0b872cf0eae403bc643dfdc5c48
J2bT <67165942+J2bT@users.noreply.github.com>
2026-06-07 19:41:55 +0400

    Make it work with PyInstaller

--- lib/pathhack.py
+++ lib/pathhack.py
@@ -1,7 +1,9 @@
+import os
 from pathlib import Path
 import sys
 
 
 # Add submodule root to sys.path
-submodule_root = Path(__file__).parent /  "unrpyc"
-sys.path.insert(0, str(submodule_root.resolve()))
+base_path = Path(sys._MEIPASS) if getattr(sys, 'frozen', False) else Path(__file__).resolve().parent
+unrpyc_path = base_path / "unrpyc"
+sys.path.insert(0, str(unrpyc_path))
